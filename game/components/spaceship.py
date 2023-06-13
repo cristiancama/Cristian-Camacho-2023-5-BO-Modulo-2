38e806c470f0940af70628e397ed7e93c8b54241
cristiancama <cristian.camacho@fundacion-jala.org>
2023-06-13 11:56:21 -0500

    Tarea 1

--- game/components/spaceship.py
+++ game/components/spaceship.py
@@ -1,7 +1,7 @@
 import pygame
 from pygame.sprite import Sprite
 
-from game.utils.constants import SPACESHIP
+from game.utils.constants import SPACESHIP, SCREEN_WIDTH
 
 # casi Todo en pygame es un objeto
 # Un personaje en mi juego es un objeto (instancia de algo)
@@ -19,8 +19,18 @@ class SpaceShip(Sprite):
         self.image_rect = self.image.get_rect()
         self.image_rect.x = self.image_size[0]
         self.image_rect.y = self.image_size[1]
+        self.velocity = 5 # Se creo una variable para la velocidad
 
     def update(self):
-        pass
+        keys = pygame.key.get_pressed() # Esta linea almacena en la variable keys lo que se preciona en el teclado, booleano
+
+        if keys[pygame.K_RIGHT]:  # Verifica si la tecla "right" esta siendo presionada, si es true incremeta la posición x por la velocidad (self.velocity)
+            self.image_rect.x += self.velocity
+        if keys[pygame.K_LEFT]:
+            self.image_rect.x -= self.velocity
+        if self.image_rect.right > SCREEN_WIDTH: # Verifica si el lado derecho de la imagen de la nave ha alcanzado el limite de la pantalla(SCREEN_WIDTH), posición left x = 0
+            self.image_rect.left  = 0
+        if self.image_rect.left < 0:
+            self.image_rect.right = SCREEN_WIDTH
 
 
